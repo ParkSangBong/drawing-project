c96ee03d113e92f80509ebd6f3079618f1f2044b
ParkSangBong <meca231@gmail.com>
2026-01-14 22:48:13 +0900

    feat: 전체 시스템 통합 및 실시간 도면 상태 대시보드 구현

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);  
-    
+    app.enableCors({
+        origin: '*',
+    })
     const config = new DocumentBuilder()
         .setTitle('Drawing Service Project API')
         .setDescription('도면 변환 플랫폼 API 명세서')
@@ -17,6 +19,16 @@ async function bootstrap() {
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// const app = await NestFactory.create(AppModule);
+  
+//   // CORS 허용 설정 추가
+//   app.enableCors({
+//     origin: '*', // 실무에서는 특정 도메인만 허용하지만, 현재는 테스트를 위해 전체 허용
+//   });
+
+//   // ... 기존 Swagger 설정 ...
+//   await app.listen(3000);
 
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
