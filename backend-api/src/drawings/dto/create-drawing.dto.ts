ca30545a84c2239e29183f2be6ea1f8945c85284
ParkSangBong <meca231@gmail.com>
2026-02-05 23:06:34 +0900

    chore: 파일명 예외

--- backend-api/src/drawings/dto/create-drawing.dto.ts
+++ backend-api/src/drawings/dto/create-drawing.dto.ts
@@ -2,7 +2,8 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const CreateDrawingSchema = z.object({
-  fileName: z.string().min(1).describe('도면 파일명'),
+  // fileName: z.string().min(1).describe('도면 파일명'),
+  fileName: z.string().optional().describe('도면 파일명 (없으면 파일 원본 이름 사용)'),
 });
 
 export class CreateDrawingDto extends createZodDto(CreateDrawingSchema) {}
